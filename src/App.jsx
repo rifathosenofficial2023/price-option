7d1272a4c48c5298570a2a56deaf4212aa306386
programmingtuns <programmingtuns@gamil.com>
2024-09-02 15:18:52 +0600

    price option project started..

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-6xl text-center bg-orange-700 p-4 m-4 rounded text-black "> Price Option</h1>
+    </div>
+  );
+}
+
+export default App;
